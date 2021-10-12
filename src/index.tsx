566f9a61d891ea40f38e5769b62b627492b11d0d
Sammy Fattah <sammy.fattah@zonal.co.uk>
2021-10-12 18:00:26 +0100

    Setup chakra provider and added quicksand font

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+import "@fontsource/quicksand";
+
+const theme = extendTheme({
+  fonts: {
+    heading: "Quicksand",
+    body: "Quicksand",
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root"),
 );
