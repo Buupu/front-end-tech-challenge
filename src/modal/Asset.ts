baad9a926acafef883052c20fd3fd0279ac7337b
Sammy Fattah <sammy.fattah@zonal.co.uk>
2021-10-13 16:54:29 +0100

    Added SearchCollectionModal

--- src/modal/Asset.ts
+++ src/modal/Asset.ts
@@ -3,11 +3,20 @@ export interface AssetModal {
     nasa_id: string;
     title: string;
     links: Link[];
+  }[];
+}
+
+export interface SearchCollectionModal {
+  collection: {
+    items: AssetModal[];
+    links: Link;
+    metadata: { total_hits: number };
   };
 }
 
 interface Link {
   href: string;
   rel: string;
-  render: string;
+  render?: string;
+  prompt?: string;
 }
